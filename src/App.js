5c338ebce244069dc982788e495da0c3f1f49879
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-09-22 14:09:08 +0600

    Modify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 		<Mobile></Mobile>
+		<Todos></Todos>
     </div>
   );
 }
@@ -33,6 +34,33 @@ function Mobile(){
 			<button onClick={decreaseValue}>Battery Down</button>
 			<button onClick={increaseCharge}>Battery Up</button>
 			<button onClick={reset}>Full Charge</button>
+		</div>
+	)
+}
+
+//Create New API Load Componenet
+function Todos(){
+	const [todos, setTodo] = useState([''])
+	useEffect(()=>{
+		fetch('https://jsonplaceholder.typicode.com/todos')
+		.then(res=>res.json())
+		.then(data=>setTodo(data))
+	},[])
+	return (
+		<div className="container">
+			{
+				todos.map(todo=><Showdata userid={todo.userId} title={todo.title}></Showdata>)
+			}
+		</div>
+	)
+}
+
+
+function Showdata(props){
+	return (
+		<div>
+			<h3>UserId: {props.userid}</h3>
+			<p>Title: {props.title}</p>
 		</div>
 	)
 }
